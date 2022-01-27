1664cc5d0b0ed8687b59263c5490b920ae9580d2
Yash Goyal <yashdineshgoyal@gmail.com>
2022-01-27 19:33:44 +0530

    changes

--- Encryption/pycrypter.py
+++ Encryption/pycrypter.py
@@ -6,7 +6,7 @@ from readBlockFile import *
 from AES256 import encrypt, decrypt
 
 
-def encryptFile(filename, keyfile, outputFile):
+def encryptFile(filename, keyfile):
     plainBlock = getLargeBlock(filename)
     encryptedBlock = []
     lenFill = len(str(len(plainBlock)))
@@ -16,15 +16,16 @@ def encryptFile(filename, keyfile, outputFile):
         print("Encrypted: " + str(i).zfill(lenFill)+"/" +
               str(len(plainBlock)).zfill(lenFill)+"\r")  # check
     print("Encrypted: "+str(len(plainBlock)))
+    return encryptedBlock
 
-    boolWritten = writeToOutputHex(encryptedBlock, outputFile)
-    if boolWritten == True:
-        return "Successfully encrypted: " + filename + " with key: " + keyfile + " into: " + outputFile
-    else:
-        return "Something went wrong, please check settings"
+    # boolWritten = writeToOutputHex(encryptedBlock, outputFile)
+    # if boolWritten == True:
+    #     return "Successfully encrypted: " + filename + " with key: " + keyfile + " into: " + outputFile
+    # else:
+    #     return "Something went wrong, please check settings"
 
 
-def decryptFile(filename, keyfile, outputFile):
+def decryptFile(filename, keyfile):
     inputBlock = getLargeHexBlock(filename)
     boolWritten = False
     for i in range(len(inputBlock)):
@@ -76,32 +77,37 @@ def is_valid_file(parser, arg):
 
 
 def Main():
-    parser = ArgumentParser(description='''
-    Parser''', formatter_class=RawDescriptionHelpFormatter)
-    parser.add_argument('operation', metavar="operation",
-                        type=str, help="Either input (Encrypt/Decrypt) or (E/D)")
-    parser.add_argument('filename', metavar="filename", type=lambda x: is_valid_file(
-        parser, x), help="Filename for file to be encrypted ex: blockfile.txt")
-    parser.add_argument('keyfile', metavar="keyfile", type=lambda x: is_valid_file(
-        parser, x), help="Keyfile with 32 byte key ex: keyfile.txt")
-    parser.add_argument('-o', '--output', metavar="filename", type=str,
-                        help="Output filename for encrypted or decrypted file default: output.txt")
-    args = parser.parse_args()
+    # parser = ArgumentParser(description='''
+    # Parser''', formatter_class=RawDescriptionHelpFormatter)
+    # parser.add_argument('operation', metavar="operation",
+    #                     type=str, help="Either input (Encrypt/Decrypt) or (E/D)")
+    # parser.add_argument('filename', metavar="filename", type=lambda x: is_valid_file(
+    #     parser, x), help="Filename for file to be encrypted ex: blockfile.txt")
+    # parser.add_argument('keyfile', metavar="keyfile", type=lambda x: is_valid_file(
+    #     parser, x), help="Keyfile with 32 byte key ex: keyfile.txt")
+    # parser.add_argument('-o', '--output', metavar="filename", type=str,
+    #                     help="Output filename for encrypted or decrypted file default: output.txt")
+    # args = parser.parse_args()
 
-    blockFile = args.filename
-    keyFile = args.keyfile
-    operation = str(args.operation).lower()
+    blockFile = input("Enter text to encrypt: ")
+    keyFile = "576E5A7234753778214125442A472D4B614E645267556B58703273357638792F"
+    # operation = str(args.operation).lower()
     status = ""
+    status = encryptFile(blockFile, keyFile)
+    status = decryptFile(blockFile, keyFile)
 
-    if not args.output:
-        output = "output.txt"
-    else:
-        output = args.output
-    startTime = datetime.now()
-    if operation == "encrypt" or operation == "e":
-        status = encryptFile(blockFile, keyFile, output)
-    elif operation == "decrypt" or operation == "d":
-        status = decryptFile(blockFile, keyFile, output)
-    executionTime = datetime.now() - startTime
+    # if not args.output:
+    #     output = "output.txt"
+    # else:
+    #     output = args.output
+    # startTime = datetime.now()
+    # if operation == "encrypt" or operation == "e":
+    #     status = encryptFile(blockFile, keyFile, output)
+    # elif operation == "decrypt" or operation == "d":
+    #     status = decryptFile(blockFile, keyFile, output)
+    # executionTime = datetime.now() - startTime
 
-    print(status + "\nExecution time: " + str(executionTime))  # check
+    # print(status + "\nExecution time: " + str(executionTime))  # check
+
+
+Main()
--- Encryption/readBlockFile.py
+++ Encryption/readBlockFile.py
@@ -1,37 +1,37 @@
-def getLargeHexBlock(filename):
+def getLargeHexBlock(hexStr):
     """
     Method to convert an encrypted file containing blockdata in Hex to integer array
     :param filename: input the filename containing string to convert to block
     :return: Integer array from blockfile
     """
-    hexFile = open(filename, 'r')
-    hexStr = hexFile.read()
+    # hexFile = open(filename, 'r')
+    # hexStr = hexFile.read()
     tempArray = []
-    blockArray =[]
+    blockArray = []
 
-    for i in range(0,len(hexStr), 2):
-        tempArray.append(int(hexStr[i:i+2],16))
+    for i in range(0, len(hexStr), 2):
+        tempArray.append(int(hexStr[i:i+2], 16))
 
-    for j in range(0,len(tempArray), 16):
+    for j in range(0, len(tempArray), 16):
         blockArray.append(tempArray[j:j+16])
     return blockArray
 
 
-def getLargeBlock(filename):
+def getLargeBlock(blockFile):
     '''
     Method for reading a file to be encrypted
     :param filename: Input of the filename to be encrypted
     :return: Integer Nested Array list with 16Byte blocks
     '''
-    blockFile = open(filename, 'r')
-    str = blockFile.read().encode("hex")
+    # blockFile = open(filename, 'r')
+    str = blockFile.encode("utf-8").hex()
     tempArray = []
     blockArray = []
 
-    for i in range(0,len(str),2):
-        tempArray.append(int(str[i:i+2],16))
+    for i in range(0, len(str), 2):
+        tempArray.append(int(str[i:i+2], 16))
 
-    if (len(tempArray)<16):
+    if (len(tempArray) < 16):
         arr = [0]*16
         n = 0
         for item in tempArray:
@@ -39,10 +39,10 @@ def getLargeBlock(filename):
             n += 1
         blockArray.append(arr)
     else:
-        while(len(tempArray)>=16):
+        while(len(tempArray) >= 16):
             blockArray.append(tempArray[0:16])
             tempArray = tempArray[16:]
-            print "Reading file \r",
+            print("Reading file \r")
             if (len(tempArray) < 16 and len(tempArray) > 0):
                 arr = [0]*16
                 n = 0
