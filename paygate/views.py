dcb6713f50ca2c3f17260862bc33afd8f9375aab
Yash Goyal <yashdineshgoyal@gmail.com>
2022-03-16 21:42:17 +0530

    Revert "merge error"
    This reverts commit 602b2f5c9a22fd23399e7afddd8626d5744154bc.

--- paygate/views.py
+++ paygate/views.py
@@ -17,7 +17,7 @@ def features(request):
 
 
 def doc(request):
-    return render(request, 'documentation.html')
+    return render(request, 'api-documentation.html')
 
 
 def sender_bank(aes_ecc_key, aes_data):
