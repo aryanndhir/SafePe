59b49c16224c34dba2485465ac706a619fbb935e
Aryann Dhir <aryanndhir.1@gmail.com>
2022-03-22 23:02:03 +0530

    static location update

--- safe_pay/settings.py
+++ safe_pay/settings.py
@@ -157,7 +157,7 @@ STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'paygate/static'),
 )
 
 #  Add configuration for static files storage using whitenoise
